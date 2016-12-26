26301d66ea1a5ed5e0704ec87d2808802c019259
Taras Postument <trane9991@gmail.com>
2016-12-27 00:16:01 +0200

    added more tests

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 # SHELL := /bin/bash
 APP_NAME := $(shell basename "$(CURDIR)")
 BRANCH_NAME ?= $(shell git rev-parse --abbrev-ref HEAD)
+GOREPO := /go/src/github.com/trane9991/cruser
 NO_COLOR=\033[0m
 OK_COLOR=\033[32;01m
 ERROR_COLOR=\033[31;01m
@@ -19,6 +20,10 @@ image: build
 tests:
 	@echo "$(OK_COLOR)+++++ Running tests +++++$(NO_COLOR)"
 	go test -v ./...
+
+docker.tests:
+	@echo "$(OK_COLOR)+++++ Running tests in Docker container, to keep your system clean from extra users +++++$(NO_COLOR)"
+	docker run -it -v $(CURDIR):${GOREPO} -e IN_DOCKER=true --workdir ${GOREPO} golang make tests
 
 demo: image
 	@echo "$(OK_COLOR)+++++ Running very simple tests in docker container +++++$(NO_COLOR)"
--- core/core_test.go
+++ core/core_test.go
@@ -4,8 +4,11 @@ import (
 	"reflect"
 	"testing"
 
+	"os"
+
 	"github.com/trane9991/cruser/core/keys"
 	"github.com/trane9991/cruser/core/user"
+	"github.com/trane9991/cruser/core/utils"
 )
 
 var testKeys = keys.SSHKeys{keys.SSHKey{
@@ -32,6 +35,12 @@ var testKeys = keys.SSHKeys{keys.SSHKey{
 	Type:     "ssh-rsa",
 	Email:    "hello@gmail.com",
 	Username: "hello",
+}, keys.SSHKey{
+	Line:     "ssh-dss ssssssssssssssssss new@user.com",
+	Key:      "ssssssssssssssssss",
+	Type:     "ssh-dss",
+	Email:    "new@user.com",
+	Username: "new",
 }}
 
 var expectedUsers = users{user.Profile{
@@ -42,6 +51,10 @@ var expectedUsers = users{user.Profile{
 	Name:              "hello",
 	SSHAuthorizedKeys: []string{"ssh-rsa cccccccccccccccccccccc hello@gmail.com", "ssh-rsa cccccccccccccccccccccc hello@gmail.com"},
 	Comment:           "hello@gmail.com;hello@gmail.com;",
+}, user.Profile{
+	Name:              "new",
+	SSHAuthorizedKeys: []string{"ssh-dss ssssssssssssssssss new@user.com"},
+	Comment:           "new@user.com",
 }}
 
 func TestKeys2Users(t *testing.T) {
@@ -55,5 +68,68 @@ func TestKeys2Users(t *testing.T) {
 				t.Fail()
 			}
 		}
+	}
+}
+
+func TestGetUsersFromFileWithKeys(t *testing.T) {
+	us := getUsersFromFileWithKeys("../users")
+	for _, u := range us {
+		for i, expectedUser := range expectedUsers {
+			if reflect.DeepEqual(u, expectedUser) {
+				break
+			}
+			if i == len(expectedUsers) {
+				t.Fail()
+			}
+		}
+	}
+
+}
+
+func TestUsersCreate(t *testing.T) {
+	if os.Getenv("IN_DOCKER") != "true" {
+		t.Logf("No variable 'IN_DOCKER' specified. We don't want to create extra users on your system, so skipping this test...")
+		t.Skip()
+	}
+	path := "../users"
+
+	lines := utils.ReadLinesFromFile(path)
+	sks := keys.Parse(lines)
+	us := keys2users(sks)
+
+	for _, u := range us {
+		if u.Exists() {
+			t.Logf("User '%s' exist, but he should... Fail", u.Name)
+			t.Fail()
+		}
+	}
+
+	dryRun := false
+	Run(&path, &dryRun)
+
+	for _, u := range us {
+		if !u.Exists() {
+			t.Logf("User '%s' not exist, but he should... Fail", u.Name)
+			t.Fail()
+		}
+		t.Logf("")
+		t.Logf("User '%s' exist", u.Name)
+		lines := utils.ReadLinesFromFile(path)
+		sks := keys.Parse(lines)
+		t.Log("Checking SSH keys...")
+
+		for _, sk := range sks {
+			for i, usk := range u.SSHAuthorizedKeys {
+				if sk.Line == usk {
+					t.Logf("SSH key '%s' exist", usk)
+					break
+				}
+				if i == len(usk) {
+					t.Logf("SSH couldn't find match for SSH key: '%s", usk)
+					t.Fail()
+				}
+			}
+		}
+
 	}
 }
